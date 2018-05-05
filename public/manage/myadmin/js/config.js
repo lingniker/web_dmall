c38fbb3f0e4daa0501ad85caceca45442fd3f714
lingyuanping <lingyuanping@outlook.com>
2018-05-05 22:02:44 +0800

    update

---
+++ public/manage/myadmin/js/config.js
@@ -0,0 +1,55 @@
+// (function(){
+// var url = "http://140.143.25.27:8080/";
+// var url = "http://192.168.1.109:8080/";
+// var url = "http://192.168.1.108:8080/";
+var url = "http://47.105.54.146:8080/";
+var mykey = "union_supermarket_2018";
+
+var tokenFlag = sessionStorage.access_token == undefined ||sessionStorage.access_token == "undefined";
+if(tokenFlag){
+    location.href="login.html";
+    console.log("跳转2");
+};
+
+
+//退出登陆按钮
+$(".back-login").on("click",function(){
+    sessionStorage.access_token = undefined;
+    location.href="login.html";
+});
+
+/*
+*对象key值排序
+*@paremet 对象
+*/
+function srotKeyObj(obj){
+    var arr = [];
+    var newobj = {};
+    for(var key in obj){
+        arr.push(key);
+    }
+    arr.sort();
+    var i=0;
+    for(var key in obj){
+        newobj[arr[i]] = obj[arr[i]];
+        i++;
+    };
+    return newobj;
+};
+/*
+*加密获取签名
+*@paramet 对象
+*/
+function objmd5(obj){
+    var str = '';
+    for(var key in obj){
+        str +=  key +"="+ obj[key] + "&";
+    }
+    var newstr = str.substr(0,str.length-1);
+    var encodeStr = encodeURIComponent(newstr) + mykey;
+    var sign = md5(encodeStr);
+    newstr += "&sign=" + sign;
+    return newstr;
+};
+
+// })();
